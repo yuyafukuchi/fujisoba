b2c6398821c4501a7fbb031e3a60f9570caff7c7
oshimayuichi <oshima@dryout.co.jp>
2017-10-27 11:48:23 +0000

    Fixed gitignore

--- config/environments.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    define('DB_HOST', 'matome-db.cvnvy2wx0t4g.ap-northeast-1.rds.amazonaws.com');
-    define('DB_PORT', 3306);
-    define('DB_USERNAME', 'fujisoba-c9');
-    define('DB_PASSWORD', 'Ee8rz8XD');
-    define('DB_DATABASE', 'fujisoba-c9-yugen');
-    define('DB_TIMEZONE', 'Asia/Tokyo');
-
